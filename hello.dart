f2c40148dde1a067a4732956f01ba0719b22c3e8
Rohan Biswas <brohan275@gmail.com>
2025-12-16 17:13:35 +0530

    commit-3

--- hello.dart
+++ hello.dart
@@ -1,3 +1,15 @@
 void main() {
   print("Hello");
+  print(3 + 3);
+  print('3' + '2');
+  // Bodmas rule is followed
+  /*
+  This
+  is 
+  multi
+  line
+  comment
+  */
 }
+
+// Printing Numbers and Letters possible
---
+++ variables.dart
@@ -0,0 +1,27 @@
+void main() {
+  print(19);
+  // No keywords, numbers in names
+
+  // Any
+  var name = "John";
+  // String
+  String name1 = "John";
+  // Integer
+  int x = 441;
+  print(x);
+  print(name1);
+  print(name);
+
+  // Dynamic
+  dynamic firstName = "Rohan";
+  print(firstName);
+
+  // const and final
+  const String fullname = "John Doe";
+  final String name2 = "John Doe";
+  print(fullname);
+  print(name2);
+
+  
+
+}
